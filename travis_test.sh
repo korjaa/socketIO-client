119e96e447b8aed7753bc5c7cf5f222c7c71f965
Juho Hovila <juho.hovila@arm.com>
2018-08-20 13:15:59 +0300

    End earlier version server

--- travis_test.sh
+++ travis_test.sh
@@ -6,8 +6,12 @@ VERSION=1.7.4
 echo "Installing socket.io version $VERSION"
 npm install -G socket.io@$VERSION
 DEBUG=* node socketIO_client/tests/serve.js &
+ID=$!
 sleep 1
 nosetests
+
+echo "Stopping node server with PID $ID"
+kill $ID
 
 VERSION=2.1.1
 echo "Installing socket.io version $VERSION"
