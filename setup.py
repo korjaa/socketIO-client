6047fb89b3d08e9b94f20097247249c5e13898c1
Jaakko Korhonen <jaakko.korhonen@arm.com>
2019-03-19 13:57:26 +0200

    Update dependency links to PEP 508.
    https://www.python.org/dev/peps/pep-0508/

--- setup.py
+++ setup.py
@@ -25,13 +25,10 @@ setup(
     author='Roy Hyunjin Han',
     author_email='rhh@crosscompute.com',
     url='https://github.com/invisibleroads/socketIO-client',
-    dependency_links=[
-        'git+https://github.com/ARMmbed/websocket-client@v0.51.0-1#egg=websocket-client-0.51.0-1'
-    ],
     install_requires=[
         'requests>=2.7.0',
         'six',
-        'websocket-client==0.51.0-1'
+        'websocket-client @ https://github.com/ARMmbed/websocket-client/archive/v0.51.0-1.zip'
     ],
     tests_require=[
         'nose',
